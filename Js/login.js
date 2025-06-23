0a1d0f4cbb9e662107a3609a32d13c598736d3cb
samidzaman <saqmidzaman@gmail.com>
2025-06-23 23:33:08 +0600

    first

--- Js/login.js
+++ Js/login.js
@@ -1,14 +1,14 @@
- document.getElementById('btn-login').addEventListener('click', function(event){
+document.getElementById('btn-login').addEventListener('click', function (event) {
     event.preventDefault()
-    
-    const PhoneNumber = document.getElementById('phone-number').value ;
-    const PinNumber = document.getElementById('pin-number').value ;
 
-    if(PhoneNumber === '01322013512' && PinNumber === '578023' ){
-       console.log("loged in") 
+    const PhoneNumber = document.getElementById('phone-number').value;
+    const PinNumber = document.getElementById('pin-number').value;
+
+    if (PhoneNumber === '01322013512' && PinNumber === 578023) {
+        console.log("loged in")
         window.location.href = 'home.html'
     }
-    else{
+    else {
         alert('wrong info')
     }
- })
+})
